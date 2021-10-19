62c217ee0dc13a49791fb67e921f5608ef55f8e7
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-10-19 18:57:49 +0200

    Start working on the searchBar

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,12 +1,24 @@
 const vue = new Vue({
   data: () => {
     return {
+      inputType: "",
       wines: [],
+      searchKey: "",
     };
+  },
+  computed: {
+    search() {
+      return this.wines.filter((wine) => {
+        return wine.name.toLowerCase().includes(this.searchKey.toLowerCase());
+      });
+    },
   },
   methods: {
     removeItem(id) {
       this.$delete(this.wines, id);
+    },
+    getImgUrl(pic) {
+      return "assets/uploads/" + pic;
     },
   },
   mounted() {
